e2d856f9cb09b6acf5b2f380ad834290a2af9455
Lael Medina <medinalael10@gmail.com>
2023-08-20 02:55:31 -0600

    version 1.1 practica 2

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using programs.Models;
+using Newtonsoft.Json;
+
+namespace programs.Controllers;
+public class CharacterController : Controller
+{
+    public List<Character> lstCharacters = null;
+    public CharacterController()
+    {
+        var myJsonString = System.IO.File.ReadAllText("Models/Characters.json");
+        lstCharacters = JsonConvert.DeserializeObject<List<Character>>(myJsonString);
+    }
+
+    public IActionResult Index()
+    {
+        return View(lstCharacters);
+    }
+
+    public IActionResult Find(string character)
+    {
+        List<Character> lstResultCharacter = new List<Character>();
+
+        //pending, solve the problem about empty strings
+        try
+        {
+            foreach (var item in lstCharacters)
+            {
+                if (item.Name.ToLower().Contains(character.ToLower()))
+                {
+                    lstResultCharacter.Add(item);
+                }
+            }
+            return View("Index", lstResultCharacter);
+        }
+        catch (NullReferenceException ex)
+        {
+            return View("Index", lstCharacters);
+        }
+    }
+
+    public IActionResult Details(int id)
+    {
+        foreach (var item in lstCharacters)
+        {
+            if (item.Id == id)
+            {
+                return View(item);
+            }
+        }
+
+        return View(new Character());
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace programs.Models;
+public class Character
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Birthday { get; set; }
+    public string? Age { get; set; }
+    public string? Gender { get; set; }
+    public string? Occupation { get; set; }
+    public string? Height { get; set; }
+    public string? Weight { get; set; }
+    public string? Calification { get; set; }
+    public string? Affiliation { get; set; }
+    public string? Poster { get; set; }
+
+    public string[]? Images { get; set; }
+}
